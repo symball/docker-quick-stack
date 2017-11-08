3cbe8c4d1c5c6a319527d26dd0574b6aaa24cd7c
Simon <simon.ball.1985@gmail.com>
2017-11-09 02:19:53 +0800

    clean file paths

--- platform_php7_developer/usergroup.sh
+++ platform_php7_developer/usergroup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-GROUP_ID=$1
+USER_ID=$1
+GROUP_ID=$2
+
+# Check if the platform group exists and configure accordingly
 if grep -q "^:$GROUP_ID:" /etc/group
     then
-         echo "group exits"
          groupmod -n platform $GROUP_ID
     else
         if grep -q "platform" /etc/group; then
@@ -12,3 +14,7 @@ if grep -q "^:$GROUP_ID:" /etc/group
          addgroup -g $GROUP_ID platform
        fi
     fi
+
+# Check whether the user exists and simply mod the ID if so
+id -u php &>/dev/null || adduser -D -h /home/php -s /usr/bin/bash -G platform -u $USER_ID php
+usermod -u $USER_ID -g $GROUP_ID php
