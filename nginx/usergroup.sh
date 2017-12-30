8ca3d5242de0b2fcde4febb7802114a5408ceaf8
Simon <simon.ball.1985@gmail.com>
2017-12-30 12:50:19 +0800

    update for simpler developer support better usergroup support with control over usernames / groups / home paths to work better with externally built containers Add developer mode for Nginx Switch port for nginx when in developer mode because regular users can't expose port 80

--- nginx/usergroup.sh
+++ nginx/usergroup.sh
@@ -3,19 +3,39 @@
 USER_ID=$1
 GROUP_ID=$2
 
-# Check if the platform group exists and configure accordingly
+if [ -z "$3" ]
+    then
+        USER_NAME="developermode"
+else
+    USER_NAME=$3
+fi
+
+if [ -z "$4" ]
+    then
+        GROUP_NAME="$USER_NAME"
+else
+    GROUP_NAME=$4
+fi
+
+if [ -z "$5" ]
+    then
+        USER_HOME="/home/$USER_NAME"
+else
+    USER_HOME=$5
+fi
+
 if grep -q "^:$GROUP_ID:" /etc/group
     then
         echo "group found by ID, making sure correct"
-         groupmod -n $GROUP_ID nginx
+         groupmod -n $GROUP_ID $GROUP_NAME
     else
-        if grep -q "nginx" /etc/group; then
+        if grep -q "$GROUP_NAME" /etc/group; then
          echo "group exists"
        else
-         addgroup -g $GROUP_ID nginx
+         addgroup -g $GROUP_ID $GROUP_NAME
        fi
     fi
 
 # Check whether the user exists and simply mod the ID if so
-id -u nginx &>/dev/null || adduser -D -h /var/cache/nginx -s /usr/bin/bash -G nginx -u $USER_ID nginx
-usermod -u $USER_ID -g nginx nginx
+id -u $USER_NAME &>/dev/null || adduser -D -h $USER_HOME -s /usr/bin/bash -G $GROUP_NAME -u $USER_ID $USER_NAME
+usermod -u $USER_ID -g $GROUP_NAME $USER_NAME
